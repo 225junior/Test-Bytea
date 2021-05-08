e6df48fd071ac107f7c54ae519452f7586eb8bbb
225junior <225juniorbrindou@gmail.com>
2021-05-08 15:36:45 +0000

    creeation de usertable de livewire: gestionnaire des files

---
+++ app/Http/Livewire/UsersTable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+
+class UsersTable extends Component
+{
+    public string $search = '';
+
+    public function render()
+    {
+        $users = User::where('name', 'LIKE', "%".$this->search."%")->get();
+        return view('livewire.users-table',\compact('users'));
+    }
+}
